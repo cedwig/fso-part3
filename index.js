1b9807a2a7c626ec36e2248c01895d72d74da5c3
Ludwig.eth <ludwig.eth@gmail.com>
2024-03-10 19:39:37 +0100

    Solution part3.6

--- index.js
+++ index.js
@@ -54,6 +54,37 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+const findPerson = (personToCheck) => {
+    return persons.find(person => person.name === personToCheck)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    const randomId = Math.floor(Math.random() * 1337)
+
+    if(findPerson(body.name)) {
+        return response.status(400).json({
+            error: `${body.name} is already in the phonebook, names must be unique`
+        })
+    }
+  
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+            error: 'A new entry must have a name and a number' 
+        })
+    }
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: randomId,
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
