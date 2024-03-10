7d8839f47c309edae13722e4388a32b2bde1731f
Ludwig.eth <ludwig.eth@gmail.com>
2024-03-10 21:31:12 +0100

    Solution part 3.7

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
 app.use(express.json())
+app.use(requestLogger)
+app.use(morgan('tiny'))
+
 
 let persons = [
     { 
@@ -84,6 +97,12 @@ app.post('/api/persons', (request, response) => {
   
     response.json(person)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
