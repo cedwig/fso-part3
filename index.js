db4d6d4e64bd5acd8310b25de2ea7042de8ea39d
Ludwig.eth <ludwig.eth@gmail.com>
2024-03-18 20:52:21 +0100

    Solution part 3.9

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 
 const requestLogger = (request, response, next) => {
@@ -15,6 +16,8 @@ morgan.token('postData', (req) => {
     return JSON.stringify(req.body)
 })
 
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
@@ -108,7 +111,7 @@ const unknownEndpoint = (request, response) => {
   
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
